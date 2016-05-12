5e172cf1e0cc3fc8fff8ddfda1a01e1ba96412fa
Nafis Hossain <nafis@labs.im>
2016-05-12 16:16:49 +1000

    add video creation and image postprocessing

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ all: clean run
 clean:
 	rm -f img/*.png
 	rm -f frames/*.png
+	rm -f out.mp4
 
 run:
 	python app.py
--- app.py
+++ app.py
@@ -12,27 +12,46 @@ def get_image():
     tz = pytz.timezone('UTC')
 
     now = dt.datetime.now(tz)
-    now = now.replace(hour=23,day=5)
+    now = now.replace(minute=int(str(now.minute)[:1] + '0'))
+    now = now.replace(hour=1,day=12)
 
     frame_num = 0
     frame_length = dt.timedelta(minutes=10)
     for i in range(10):
         img_num = 0
-        for y in range(13, 17):
-            for x in range(10, 14):
+        for y in range(14, 16):
+            for x in range(11, 13):
                 url = gen_url(now, x, y)
-                print url
+                print(url)
                 r = requests.get(url)
                 img_num += 1
                 with open('img/' + str(img_num).zfill(2) + '.png', 'w') as png:
                     png.write(r.content)
 
-        subprocess.call(['montage', '-mode', 'concatenate', '-tile', '4x4', 
-                         'img/*.png','frames/' + str(frame_num).zfill(2) + '.png'])
+        frame_name = str(frame_num).zfill(2) + '.png'
+        subprocess.call(['montage', '-mode', 'concatenate', '-tile', '2x2', 
+                         'img/*.png','frames/' + frame_name])
+
+        subprocess.call(['convert', 'frames/' + frame_name, '-brightness-contrast', '+15x+20',
+                        'frames/' + frame_name])
+
+        qld = pytz.timezone('Australia/Queensland')
+        q_time = now.astimezone(qld)
+
+        subprocess.call(['convert', 'frames/' + frame_name,
+        '-fill', 'white', '-pointsize', '18', '-undercolor',
+        '#00000080', '-gravity', 'South', '-annotate', '+0+5',
+        q_time.strftime('%H:%M %Z'), 'frames/' + frame_name])
+
         frame_num += 1
         now = now + frame_length
         print(now)
 
+def create_video():
+    subprocess.call(['ffmpeg', '-framerate', '5', '-i', 'frames/%02d.png',
+                     '-r', '5', '-c:v', 'libx264', '-pix_fmt', 'yuv420p',
+                     'out.mp4'])
 
 if __name__ == '__main__':
     get_image()
+    create_video()
