887f47c41bd48a3b9f4973c8f499304d63db3d22
Nafis Hossain <nafis@labs.im>
2016-05-17 16:00:13 +1000

    add tracker and updated create_video function
    tracker function takes input of hours and creates frame objects
    for the number of hours from current time to the input hours.
    
    create_video now works with frame objects

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
-.PHONY: clean run
+.PHONY: clean run test
 
 all: clean run
 
 clean:
-	rm -f img/*.png
-	rm -f frames/*.png
+	rm -rf frames/*
 	rm -f out.mp4
+	rm -rf video_frames/*
 
 run:
 	python app.py
+
+test:
+	py.test test
--- app.py
+++ app.py
@@ -4,46 +4,85 @@ import requests
 import datetime as dt
 import subprocess
 import hashlib
+import binascii
+import shutil
 
 
 class SingleFrame(object):
-    def __init__(self, datetime, x_range, y_range, tmp_folder, output_path):
+    def __init__(self, datetime, x_range, y_range, frame_folder):
         self.dt = datetime
         self.x_range = x_range
         self.y_range = y_range
-        self.tmp_folder = tmp_folder
-        self._output_path = output_path
+
+        self.tmp_folder = os.path.join(frame_folder, 'img')
+        self.output_folder = os.path.join(frame_folder, 'out')
+        self._output_path = os.path.join(self.output_folder, 'frame.png')
 
         self.created = False
+        self.no_img = False
     
+        os.makedirs(self.tmp_folder)
+        os.makedirs(self.output_folder)
+
     def create_frame(self):
         if self.created:
             return
 
         img_num = 0
-        for y in self.x_range:
-            for x in self.y_range:
+        for y in self.y_range:
+            for x in self.x_range:
                 img = SingleImage(self.dt, x, y)
 
                 img_name = str(img_num).zfill(3) + '.png'
                 img_path = os.path.join(self.tmp_folder, img_name)
+                
+                if img.is_no_img():
+                    self.no_img = True
+                    return
+
                 with open(img_path, 'w') as png_file:
                     png_file.write(img.get_image())
 
-                print('wrote: ' + img_name)
-
                 img_num += 1
 
+
+        self.montage_images()
+        self.created = True
+        print('created frame: ' + self._output_path)
+
+        self.set_brightness_contrast()
+        self.add_timestamp()
+    
+    def get_frame(self):
+        self.create_frame()
+
+        if self.created:
+            return self._output_path
+
+        return False
+    
+    def is_no_img(self):
+        if self.created == False:
+            self.create_frame()
+
+        return self.no_img
+
+    def set_brightness_contrast(self):
+        subprocess.call(['convert', self.get_frame(), '-brightness-contrast',
+            '+15x+20', self.get_frame()])
+
+    def montage_images(self):
         subprocess.call(['montage', '-mode', 'concatenate', '-tile', '2x2',
             os.path.join(self.tmp_folder, '*.png'), self._output_path])
 
-        print('created frame: ' + self._output_path)
-        
+    def add_timestamp(self):
+        qld = pytz.timezone('Australia/Queensland')
+        q_time = self.dt.astimezone(qld)
 
-    
-    def get_frame(self):
-        create_frame()
-        return self._output_path
+        subprocess.call(['convert', self.get_frame(), '-fill', 'white',
+            '-pointsize', '24', '-undercolor', '#00000080', '-gravity',
+            'South', '-annotate', '+0+5', q_time.strftime('%H:%M %Z'),
+            self.get_frame()])
 
 
 class SingleImage(object):
@@ -60,21 +99,18 @@ class SingleImage(object):
             self.img = r.content
 
         return self.img
+    
+    def is_no_img(self):
+        no_img_hash = r'142dc29d84424bbd305c14168454024a1f758047'
+        img_hash = hashlib.sha1(self.get_image()).hexdigest()
+
+        return img_hash == no_img_hash
 
 
 def gen_url(datetime, x, y):
     return 'http://himawari8-dl.nict.go.jp/himawari8/img/D531106/20d/550/' +\
             datetime.strftime("%Y/%m/%d/%H") + datetime.strftime("%M")[:1] +\
             '000_' + str(x) + '_' + str(y) + '.png'
-
-def run():
-    now = dt.datetime.now(pytz.timezone('UTC'))
-    now = now.replace(hour=2)
-
-    f = SingleFrame(now, [14, 15], [11, 12], '/home/nafis/himawari8/app/img',
-            '/home/nafis/himawari8/app/frames/001.png')
-    f.create_frame()
-
 
 def get_image():
     tz = pytz.timezone('UTC')
@@ -117,18 +153,62 @@ def get_image():
         now = now + frame_length
         print(now)
 
-def check_no_img(img_path):
-    no_img_hash = r'142dc29d84424bbd305c14168454024a1f758047'
-    with open(img_path) as f:
-        h = hashlib.sha1(f.read()).hexdigest()
-        return no_img_hash == h
+def create_video(frames):
 
-def create_video():
-    subprocess.call(['ffmpeg', '-framerate', '5', '-i', 'frames/%02d.png',
-                     '-r', '5', '-c:v', 'libx264', '-pix_fmt', 'yuv420p',
-                     'out.mp4'])
+    video_frames_dir = '/home/nafis/himawari8/app/video_frames'
+
+    frame_num = 0
+
+    for frame in reversed(frames):
+
+        if frame.is_no_img():
+            continue
+
+        frame_path = frame.get_frame()
+        frame_out_path = os.path.join(video_frames_dir,
+                str(frame_num).zfill(3) + '.png')
+
+        shutil.copyfile(frame_path, frame_out_path)
+
+        frame_num += 1
+
+    subprocess.call(['ffmpeg', '-framerate', '5', '-i',
+        os.path.join(video_frames_dir, '%03d.png'), '-r', '5', '-c:v',
+        'libx264', '-pix_fmt', 'yuv420p', 'out.mp4'])
+
+def gen_rand_folder(base_path):
+    folder_name = binascii.hexlify(os.urandom(8))
+
+    folder_path = os.path.join(base_path, folder_name)
+
+    if os.path.exists(folder_path):
+        return rand_folder_gen(base_path)
+
+    return folder_path
+
+
+def tracker(hours):
+    x_range = [11, 12]
+    y_range = [14, 15]
+
+    frames_folder = '/home/nafis/himawari8/app/frames'
+
+    time_interval = dt.timedelta(minutes=10)
+    frame_time = dt.datetime.now(pytz.timezone('UTC'))
+
+    frames = []
+
+    num_frames = hours * 6
+
+    for i in range(num_frames):
+        rand_folder = gen_rand_folder(frames_folder)
+        frame = SingleFrame(frame_time, x_range, y_range, rand_folder)
+        frames.append(frame)
+
+        frame_time = frame_time - time_interval
+ 
+    create_video(frames)
+
 
 if __name__ == '__main__':
-    #get_image()
-    #create_video()
-    run()
+    tracker(2)
